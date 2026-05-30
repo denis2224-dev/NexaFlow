d066a7835927e4f66a3fc91633614d159d3b0456
Denis <denymoroz2006@gmail.com>
2026-05-30 20:29:42 +0300

    chore(gateway): generate JHipster gateway

---
+++ gateway/src/main/webapp/app/admin/gateway/gateway.ts
@@ -0,0 +1,51 @@
+import { KeyValuePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { TranslateDirective } from 'app/shared/language';
+
+import { GatewayRoute } from './gateway-route.model';
+import { GatewayRoutesService } from './gateway-routes.service';
+
+@Component({
+  selector: 'jhi-gateway',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './gateway.html',
+  providers: [GatewayRoutesService],
+  imports: [KeyValuePipe, FontAwesomeModule, TranslateDirective, TranslateModule],
+})
+export default class Gateway implements OnInit {
+  gatewayRoutes: GatewayRoute[] = [];
+  updatingRoutes = false;
+
+  private readonly gatewayRoutesService = inject(GatewayRoutesService);
+
+  ngOnInit(): void {
+    this.refresh();
+  }
+
+  refresh(): void {
+    this.updatingRoutes = true;
+    this.gatewayRoutesService.findAll().subscribe(gatewayRoutes => {
+      this.gatewayRoutes = gatewayRoutes.map(gatewayRoute => {
+        gatewayRoute.serviceInstances = gatewayRoute.serviceInstances.map(serviceInstance => {
+          if (serviceInstance?.healthService?.checks) {
+            if (
+              serviceInstance.healthService.checks.filter((check: any) => check.status === 'PASSING').length ===
+              serviceInstance.healthService.checks.length
+            ) {
+              serviceInstance.instanceInfo = { status: 'UP' };
+            } else {
+              serviceInstance.instanceInfo = { status: 'DOWN' };
+            }
+          }
+          return serviceInstance as object;
+        });
+        return gatewayRoute;
+      });
+      this.updatingRoutes = false;
+    });
+  }
+}
